4d9a18cf0406df4fad84989e89039d687ae64442
enriquehco <enrikillo26@gmail.com>
2022-11-18 14:23:00 +0100

    dummy text

---
+++ assessmentV2/assessmentV2/Models/Owner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace assessmentV2.Models;
+
+public partial class Owner
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string? LastName { get; set; }
+
+    public string DriverLicense { get; set; } = null!;
+}
