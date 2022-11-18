89ad821b9ccccf05c526846b9a57cc15a7412093
enriquehco <enrikillo26@gmail.com>
2022-11-18 12:51:20 +0100

    mvc set

---
+++ back/Models/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NETAssessment.Models;
+
+public partial class Vehicle
+{
+    public int Id { get; set; }
+
+    public string Brand { get; set; } = null!;
+
+    public string? Vin { get; set; }
+
+    public string Color { get; set; } = null!;
+
+    public string Year { get; set; } = null!;
+
+    public string OwnerId { get; set; } = null!;
+}
